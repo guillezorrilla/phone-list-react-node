5e42c5348dca3e7d88bf2ac068e96089b8150898
Guille Zorrilla <guillez94@hotmail.com>
2021-01-13 09:33:11 +0100

    loading state

--- frontend/src/Phones/components/PhoneList.js
+++ frontend/src/Phones/components/PhoneList.js
@@ -1,40 +1,35 @@
 import React from 'react';
-import Card from '../../shared/components/UIElements/Card/Card';
 import Spinner from '../../shared/components/UIElements/Spinner/Spinner';
 
 import PhoneItem from './PhoneItem';
 import './PhoneList.css';
 
 const PhoneList = (props) => {
-  if (props.isLoading) {
-    return <Spinner />;
-  }
   if (!props.items || props.items.length === 0) {
-    return (
-      <Card>
-        <h2>No se encontraton telefonos</h2>
-      </Card>
-    );
+    return <Spinner />;
   }
 
   return (
     <React.Fragment>
-      {props.isLoading && <Spinner />}
-      <ul className='phone-list'>
-        {props.items.map((phone) => {
-          return (
-            <PhoneItem
-              key={phone.id}
-              id={phone.id}
-              image={phone.imageUrl}
-              title={phone.title}
-              description={phone.description}
-              color={phone.color}
-              price={phone.price}
-            />
-          );
-        })}
-      </ul>
+      {props.isLoading ? (
+        <Spinner />
+      ) : (
+        <ul className='phone-list'>
+          {props.items.map((phone) => {
+            return (
+              <PhoneItem
+                key={phone.id}
+                id={phone.id}
+                image={phone.imageUrl}
+                title={phone.title}
+                description={phone.description}
+                color={phone.color}
+                price={phone.price}
+              />
+            );
+          })}
+        </ul>
+      )}
     </React.Fragment>
   );
 };
--- frontend/src/Phones/pages/PhoneDetailComponent.js
+++ frontend/src/Phones/pages/PhoneDetailComponent.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Card from '../../shared/components/UIElements/Card/Card';
+import Spinner from '../../shared/components/UIElements/Spinner/Spinner';
 
 import * as actions from '../../store/actions/index';
 
@@ -21,48 +22,46 @@ const PhoneDetailComponent = (props) => {
   if (!phone) {
     detailPhone = (
       <Card>
-        <h3>Cargando detalle movil</h3>
+        <h3>No se pudieron econtrar telefonos</h3>
       </Card>
     );
   } else {
     detailPhone = (
-      <React.Fragment>
-        {props.isLoading ? (
-          <Spinner />
-        ) : (
-          <div className='photo-detail__container'>
-            <div className='phone-detail__image'>
-              <img src={phone.imageUrl} alt={phone.title} />
-            </div>
-            <Card className='detail-card__container'>
-              <div className='phone-detail__info'>
-                <h2>{phone.title}</h2>
-                <h3>{phone.color}</h3>
-                <p>{phone.description}</p>
-              </div>
-              <div>
-                <h3>Especificaciones</h3>
-                <ul>
-                  {phone.especificaciones.map((e) => {
-                    return (
-                      <li key={e.name}>
-                        {e.name} : {e.value}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-              <div style={{ textAlign: 'center' }}>
-                <h2>{`Precio: ${phone.price}`}</h2>
-              </div>
-            </Card>
+      <div className='photo-detail__container'>
+        <div className='phone-detail__image'>
+          <img src={phone.imageUrl} alt={phone.title} />
+        </div>
+        <Card className='detail-card__container'>
+          <div className='phone-detail__info'>
+            <h2>{phone.title}</h2>
+            <h3>{phone.color}</h3>
+            <p>{phone.description}</p>
           </div>
-        )}
-      </React.Fragment>
+          <div>
+            <h3>Especificaciones</h3>
+            <ul>
+              {phone.especificaciones.map((e) => {
+                return (
+                  <li key={e.name}>
+                    {e.name} : {e.value}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div style={{ textAlign: 'center' }}>
+            <h2>{`Precio: ${phone.price}`}</h2>
+          </div>
+        </Card>
+      </div>
     );
   }
 
-  return <React.Fragment>{detailPhone}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {props.isLoading ? <Spinner /> : { detailPhone }}
+    </React.Fragment>
+  );
 };
 
 const mapStateToProps = (state) => {
